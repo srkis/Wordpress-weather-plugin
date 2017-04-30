ed7225ba1bc1deebd7199c777089709b46f160ac
Srdjan <stojanovicsrdjan27@gmail.com>
2017-05-01 01:49:51 +0200

    Add files via upload

---
+++ show_weather_widget.php
@@ -0,0 +1,52 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+
+    <title>Weather Widget By Srki</title>
+
+</head>
+
+<body onload="dateMonth();">
+
+<article class="weather_widget">
+
+    <div id="changeIcon"></div>
+
+    <div class="weatherIcon"></div>
+    <div class="humidity"></div>
+    <div class="wind"></div>
+    <div class="pressure"></div>
+    <div class="temp_min"></div>
+    <div class="temp_max"></div>
+
+
+    <div class="weatherData">
+        <h2 class="description"></h2>
+        <h1 class="temperature"> </h1>
+
+        <h3 class="city"> </h3>
+    </div>
+    <div class="weather_date">
+        <span id="weather_month"></span>
+        <span id="weather_day"></span>
+    </div>
+
+
+
+</article>
+
+
+</body>
+</html>
+
+<script>
+   var url ='<?php echo site_url()?>';
+
+    $(document).ready(function() {
+        userData(url);
+
+
+    });
+</script>
+
